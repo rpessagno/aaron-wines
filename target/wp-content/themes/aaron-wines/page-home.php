dca4a644441cbd42d91d854aa9feb5cc36597121
Makers & Allies <robert@makersandallies.com>
2017-09-11 16:50:53 -0700

    feature, button, hero

--- target/wp-content/themes/aaron-wines/page-home.php
+++ target/wp-content/themes/aaron-wines/page-home.php
@@ -8,4 +8,25 @@ get_header();
 
 ?>
 
+<div class="hero"></div>
+
+<div class="feature-wrap">
+  <div class="feature">
+    <div class="feature-img" style="background-image: url('https://cdn.shopify.com/s/files/1/0362/4429/t/8/assets/slideshow_2.jpg?8356872384184126241');"></div>
+    <div class="feature-content">
+      <h3 class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam commodi odio non quo facere voluptatibus quasi at sequi, voluptatum facilis deserunt quas maiores, sint, praesentium sunt tempora magnam porro corrupti.</p>
+      <a href="#" class="button">Button</a>
+    </div>
+  </div>
+  <div class="feature">
+    <div class="feature-img" style="background-image: url('https://cdn.shopify.com/s/files/1/0362/4429/t/8/assets/slideshow_2.jpg?8356872384184126241');"></div>
+    <div class="feature-content">
+      <h3 class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam commodi odio non quo facere voluptatibus quasi at sequi, voluptatum facilis deserunt quas maiores, sint, praesentium sunt tempora magnam porro corrupti.</p>
+      <a href="#" class="button">Button</a>
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
