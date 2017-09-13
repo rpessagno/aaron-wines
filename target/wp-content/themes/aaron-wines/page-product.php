85c5d334f7912933b8e11d6298d00631307e8b52
Makers & Allies <robert@makersandallies.com>
2017-09-13 16:28:45 -0700

    product page

--- target/wp-content/themes/aaron-wines/page-product.php
+++ target/wp-content/themes/aaron-wines/page-product.php
@@ -10,22 +10,21 @@ get_header();
 
 
 
-<div data-vsPlugin="productDetail" id="product" class="product">
-  <noscript>
-    JavaScript is required to view and purchase our products.<br>
-    Please <a href="http://www.enable-javascript.com/" target="_blank">enable this feature in your browser.</a>
-  </noscript>
+<div data-vsPlugin="productDetail" id="product">
 
   <script type="text/html" id="product-custom">
     <div class="product" data-bind="with: product">
       <div class="product-img">
-        <img class="product-img" data-bind="attr: { alt: Title, src: DetailImageUrl }">
+        <img data-bind="attr: { alt: Title, src: DetailImageUrl }">
       </div>
       <div class="product-content">
-        <h1 class="product-title" data-bind="text: Title"></h1>
-        <p class="product-price" data-bind="text: '$' + Price"></p>
+        <h1 class="product-title subtitle" data-bind="text: Title"></h1>
+        <p class="product-price subtitle2" data-bind="text: '$' + Price"></p>
         <div class="cart" data-bind="template: 'vs2-productList-addToCart-default'"></div>
-        <div class="product-description" data-bind="html: Description.replace(/\n/g,'<br>')"></div>
+        <div class="product-description text" data-bind="html: Description.replace(/\n/g,'<br>')"></div>
+        <ul class="vs2-productDetail-attributes text" data-bind="foreach: AttributeList &amp;&amp; AttributeList.split('\n')">
+          <li data-bind="html: $data"></li>
+        </ul>
       </div>
     </div>
   </script>
