e1953cc8307f5c4b62db39ec3a82197193c5ce05
Robert Pessagno <rpessagno@gmail.com>
2019-05-23 19:08:28 -0700

    product drilldown styles

--- winedirect-template/inc/product-photo.php
+++ winedirect-template/inc/product-photo.php
@@ -1,4 +1,4 @@
 <div class="v65-product-photo" v65js="productLargePhoto">
-  <meta itemprop="image" content="assets/images/product/bottle.png">
-  <img src="assets/images/product/bottle.png" alt="">
+  <meta itemprop="image" content="assets/images/product/Aaron-Bottle-Shot-Petite-Sirah-Detail.png">
+  <img src="assets/images/product/Aaron-Bottle-Shot-Petite-Sirah-Detail.png" alt="">
 </div>
