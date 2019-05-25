58f7b83d1ec03bee6811ed86644e4f9c860faaf5
Robert Pessagno <rpessagno@gmail.com>
2019-05-24 18:07:53 -0700

    error pages

--- winedirect-template/not-found.php
+++ winedirect-template/not-found.php
@@ -1,6 +1,6 @@
 <?php include('inc/header.php'); ?>
 
-<div class="winedirect-wrapper notfound">
+<div class="winedirect-wrapper not-found">
   <h1 class="title">Not Found</h1>
   <p class="text">The page you are looking for does not exist.</p>
   <a href="/" class="button">Back to home</a>
