a255df8038c18db14ef171d297af6f18e8608e32
Robert Pessagno <rpessagno@gmail.com>
2023-01-17 17:47:24 -0800

    home page design

--- target/wp-content/themes/aaron-wines/footer.php
+++ target/wp-content/themes/aaron-wines/footer.php
@@ -31,8 +31,8 @@
     <div class="footer-item club">
       <h1 class="footer-title">Members</h1>
       <p class="text">
-        <a href="https://shop.aaronwines.com/index.cfm?method=memberEditAccount.editProfile" class="button-outline">Sign In</a>
-        <a href="https://shop.aaronwines.com/index.cfm?method=memberCreateAccount.SignUp" class="button-outline">Join</a></p>
+        <a href="https://shop.aaronwines.com/index.cfm?method=memberEditAccount.editProfile" class="button-outline button-dark">Sign In</a>
+        <a href="https://shop.aaronwines.com/index.cfm?method=memberCreateAccount.SignUp" class="button-outline button-dark">Join</a></p>
     </div>
 
 <!--     <div class="footer-item footer-social">
--- target/wp-content/themes/aaron-wines/page-home.php
+++ target/wp-content/themes/aaron-wines/page-home.php
@@ -14,7 +14,7 @@ get_header();
 
   <div class="intro">
     <div class="intro-content">
-      <h1 class="headline"><?php the_field('intro_headline'); ?></h1>
+      <h1 class="h1"><?php the_field('intro_headline'); ?></h1>
       <p class="intro-text"><?php the_field('intro_text'); ?></p>
     </div>
   </div>
@@ -28,9 +28,11 @@ get_header();
       <img src="https://aaronwines.com/wp-content/uploads/2019/05/Aequorea-Bottle-Shot-Pinot-Noir-Derby-Detail.png">
       <img src="https://shop.aaronwines.com/assets/images/products/pictures/Aequorea-Bottle-Shot-Pinot-Gris-Detail-IUEEGM.png">
       <img src="https://aaronwines.com/wp-content/uploads/2019/05/Aaron-Bottle-Shot-Trespasser-Detail.png">
+      <img src="https://aaronwines.com/wp-content/uploads/2019/05/Aequorea-Bottle-Shot-Pinot-Noir-Seafarer-Detail.png">
+      <img src="https://shop.aaronwines.com/assets/images/products/pictures/Aequorea-Bottle-Shot-Pinot-Gris-Detail-IUEEGM.png">
     </div>
     <div class="home-shop-link">
-      <a href="#">Shop Wines</a>
+      <a href="#">Explore Our Collection</a>
     </div>
   </div>
 
@@ -58,11 +60,11 @@ get_header();
 
   <div class="feature home-visit">
     <div class="feature-content">
-      <h3 class="subtitle"><?php the_field('visit_title'); ?></h3>
+      <h2 class="h2">Visit Our <br> Tasting Room</h2>
       <p><?php the_field('visit_text'); ?></p>
-      <a href="<?php echo the_permalink('30'); ?>" class="button">Visit Us</a>
+      <a href="<?php echo the_permalink('30'); ?>" class="button-outline button-dark">Visit Us</a>
     </div>
-    <div class="feature-img" style="background-image: url('<?php the_field('visit_image'); ?>');"></div>
+    <div class="feature-img" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/home/visit.jpg');"></div>
   </div>
 
 <!--   <div class="feature home-club">
@@ -78,38 +80,38 @@ get_header();
 
 <div class="home-map">
   <div class="home-map-content">
-    <h1 class="headline">We're in Tin City</h1>
+    <h2 class="h1">In the Heart <br> of Tin City</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et vitae eveniet necessitatibus, nesciunt, consequatur, aspernatur error iusto quis atque, architecto veritatis harum molestias sit. Cumque debitis molestias, voluptates quibusdam.</p>
-    <a href="#" class="button">Get in Touch</a>
+    <a href="#" class="button-outline button-light">Learn More</a>
   </div>
 </div>
 
 
-<div class="feature-wrap home-features">
+<div class="home-club-winemaker">
 
   <div class="feature home-club">
     <div class="feature-content alt">
-      <h3 class="subtitle"><?php the_field('club_title'); ?></h3>
-      <p><?php the_field('club_text'); ?></p>
-      <a href="<?php echo the_permalink('28'); ?>" class="button">Learn More</a>
+      <h2 class="h2">Join Our <br> Wine Club</h2>
+      <p>If you love our wines as much as we do, we invite you to join our wine club and become a part of our family. Every six months, you’ll receive a shipment of our new wines, before they are released to the public.</p>
+      <a href="<?php echo the_permalink('28'); ?>" class="button-outline button-dark">Join Now</a>
     </div>
-    <div class="feature-img" style="background-image: url('<?php the_field('club_image'); ?>');"></div>
+    <div class="feature-img" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/home/club.jpg');"></div>
   </div>
 
-</div>
 
-
-<div class="home-winemaker">
-  <div class="home-winemaker-wrap">
-    <div class="home-winemaker-img">
-      <img src="<?php bloginfo('template_directory'); ?>/assets/images/home/aaron-surfboard.jpg" alt="Aaron with surfboard">
+  <div class="home-winemaker">
+    <div class="home-winemaker-wrap">
+      <div class="home-winemaker-img">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/home/aaron-surfboard.jpg" alt="Aaron with surfboard">
+      </div>
+      <blockquote class="home-winemaker-content">
+        <p>&ldquo;My wines are like my love letters to the Central Coast.</p>
+        <p>Everytime I create one, it’s me feeling prideful and honoring the place where I grew up.&rdquo;</p>
+        <cite><span>Aaron Jackson</span> <br> Owner / Winemaker</cite>
+      </blockquote>
     </div>
-    <blockquote class="home-winemaker-content">
-      <p>&ldquo;My wines are like my love letters to the Central Coast.</p>
-      <p>Everytime I create one, it’s me feeling prideful and honoring the place where I grew up.&rdquo;</p>
-      <cite><span>Aaron Jackson</span> <br> Owner / Winemaker</cite>
-    </blockquote>
   </div>
+
 </div>
 
 <div class="home-skyline">
