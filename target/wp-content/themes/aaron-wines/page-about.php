f9ab6d2eb434422e2392d8333a9063513c54f60f
Robert Pessagno <rpessagno@gmail.com>
2023-02-09 18:21:52 -0800

    about

---
+++ target/wp-content/themes/aaron-wines/page-about.php
@@ -0,0 +1,129 @@
+<?php
+
+/*
+Template Name: About
+*/
+
+get_header();
+
+?>
+
+
+
+
+
+<div class="hero page-hero" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/home/hero-2.jpg');">
+  <h1 class="hero-title">About</h1>
+</div>
+
+
+<div class="about-winemaker">
+  <div class="about-winemaker-content">
+      <h1 class="eyebrow">The Winemaker</h1>
+      <h1 class="h1">Aaron Jackson</h1>
+      <p>A Central Coast native, Aaron Jackson began his winemaking career in vineyards and cellars near his hometown of Cayucos during the 2000 vintage. In 2002, while studying wine at Cal Poly in San Luis Obispo, he pooled together the pennies from his weekend job and launched his eponymous label with the first vintage Aaron Petite Sirah. His early career was spent working with and mentoring from several wineries around Paso Robles (Saxum, Four Vines, ONX, and others) along with earning his Master’s degree in Oenology from the University of Adelaide in South Australia.</p>
+      <p>Now over 20 years in, Aaron continues to focus on bringing pride to his homeland through his two brands, both of which are driven by authenticity and devotion to unearthing the potential of Paso Robles and the San Luis Obispo Coast.</p>
+  </div>
+</div>
+
+
+<!-- <div class="home-intro">
+
+  <div class="intro">
+    <div class="intro-content">
+      <h1 class="eyebrow">The Winemaker</h1>
+      <h1 class="h1">Aaron Jackson</h1>
+      <p class="p1">A Central Coast native, Aaron Jackson began his winemaking career in vineyards and cellars near his hometown of Cayucos during the 2000 vintage. In 2002, while studying wine at Cal Poly in San Luis Obispo, he pooled together the pennies from his weekend job and launched his eponymous label with the first vintage Aaron Petite Sirah. His early career was spent working with and mentoring from several wineries around Paso Robles (Saxum, Four Vines, ONX, and others) along with earning his Master’s degree in Oenology from the University of Adelaide in South Australia.</p>
+      <p class="p1">Now over 20 years in, Aaron continues to focus on bringing pride to his homeland through his two brands, both of which are driven by authenticity and devotion to unearthing the potential of Paso Robles and the San Luis Obispo Coast.</p>
+    </div>
+  </div>
+
+</div> -->
+
+
+
+
+<div class="about-team">
+  <div class="about-team-header">
+    <h1 class="h1">Team</h1>
+  </div>
+  <div class="about-team-items">
+    <div class="about-team-item">
+      <div class="about-team-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/team/nick.png" alt=""></div>
+      <h3 class="h3">Nick Pearl</h3>
+      <p>GM / Assistant Winemaker</p>
+    </div>
+    <div class="about-team-item">
+      <div class="about-team-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/team/justin.jpeg" alt=""></div>
+      <h3 class="h3">Justin Rapolas</h3>
+      <p>Marketing & Brand Manager</p>
+    </div>
+    <div class="about-team-item">
+      <div class="about-team-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/team/katie.png" alt=""></div>
+      <h3 class="h3">Katie Lewis</h3>
+      <p>Director of Sales</p>
+    </div>
+    <div class="about-team-item">
+      <div class="about-team-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/team/nataly.png" alt=""></div>
+      <h3 class="h3">Nataly Weir</h3>
+      <p>Swiss Army Knife</p>
+    </div>
+    <div class="about-team-item">
+      <div class="about-team-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/team/amber.png" alt=""></div>
+      <h3 class="h3">Amber Sandoval</h3>
+      <p>Quesadillas</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<div class="about-labels">
+  <div class="about-labels-header">
+    <h1 class="h1">Our Labels</h1>
+  </div>
+  <div class="about-labels-items">
+    <div class="about-labels-item">
+      <div class="about-labels-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/labels/aaron.jpg" alt=""></div>
+      <h3 class="h3">Aaron</h3>
+      <p>Aaron began in 2002 with a focus on producing powerful, age-worthy wines from the rugged hillsides of westside Paso Robles, California. Always seeking to veer from the norm, we found inspiration in creating blends that centered around one of California's most historic grapes — Petite Sirah. We source intense, yet balanced fruit from the Willow Creek, Adelaida, and Templeton Gap districts. From these sites we coax this massively structured varietal into a portfolio of unique blends that evoke our best expression of west side Paso Robles terroir.</p>
+    </div>
+    <div class="about-labels-item">
+      <div class="about-labels-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/labels/aequorea.jpg" alt=""></div>
+      <h3 class="h3">Aequorea</h3>
+      <p>Latin for “Of The Sea,” Aequorea is a study in extreme coastal winegrowing, focused specifically on Pinot Noir and aromatic white wines. Our devotion to working with vineyards west of the Santa Lucia Mountains, closer to the ocean than most anywhere else in California, led to us spearheading the creation of the San Luis Obispo Coast (SLO Coast) AVA. From these foggy, windswept sites we work with dedicated growers to craft a portfolio of expressive wines that showcase truly coastal terroir, each vineyard being no more than 6 miles from the Pacific.</p>
+    </div>
+    <div class="about-labels-item">
+      <div class="about-labels-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/labels/keola.jpg" alt=""></div>
+      <h3 class="h3">Nature Boy</h3>
+      <p>A wine originally made for ourselves and industry friends, we realized it was too good not to share with all of you. Nature Boy is a 100% whole cluster, 100% carbonic maceration Grenache sourced from our favorite vineyards in the Willow Creek District in west Paso Robles. Light in body, moderate in alcohol, and packed full of aromatics and bright fruit- it is an absolute joy to drink.</p>
+    </div>
+    <div class="about-labels-item">
+      <div class="about-labels-item-img"><img src="<?php bloginfo('template_directory'); ?>/assets/images/about/labels/nature-boy.jpg" alt=""></div>
+      <h3 class="h3">Keola</h3>
+      <p>“Keola,” which is Aaron Jackson’s Hawaiian middle name, are wines comprised of grapes from the same vineyards that make up our top-tier wines. We don’t purchase inexpensive or lower quality fruit for these bends, rather it is comprised of extra juice that simply don’t find their way into our Aaron or Aequorea wines. This means that these wines always massively overdeliver for their price, with a similar pedigree and attention to detail as our flagship wines, but at a more approachable price point for everyday drinking.</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="home-map">
+  <div class="home-map-content">
+    <h2 class="h1">Giving Back</h2>
+    <p>Our love for the outdoors has inspired us to join 1% for the Planet, an international organization whose members contribute at least one percent of their annual sales to environmental causes. Through this organization, we support a local nonprofit directly that aligns with our values and brand.</p>
+    <p>Because of our passion for Big Sur, we've partnered with the <a href="https://www.ventanawild.org/" target="_blank">Ventana Wilderness Alliance</a> to protect, preserve, and restore the wilderness and public lands of Big Sur. We've specifically been supporting their amazing efforts at maintaining and rebuilding the extensive network of hiking trails and camps that have been ravaged by recent wildfires.</p>
+  </div>
+</div>
+
+
+
+<div class="about-skyline">
+  <img src="<?php bloginfo('template_directory'); ?>/assets/images/home/celebration.jpg" alt="">
+</div>
+
+<?php get_footer(); ?>
