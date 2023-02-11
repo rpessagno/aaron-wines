84900f8acc27c28c45d898a22f2ce3e65d052ee0
Robert Pessagno <rpessagno@gmail.com>
2023-02-11 14:07:46 -0800

    about ideas

---
+++ src/js/src/labels-slider.js
@@ -0,0 +1,15 @@
+//==============================
+// Labels Slider
+//==============================
+
+$('.about-labels-items').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // centerMode: true,
+  // centerPadding: '15%',
+  speed: 800,
+  infinite: false,
+  prevArrow: '<button type="button" class="slick-prev">←</button>',
+  nextArrow: '<button type="button" class="slick-next">→</button>'
+});
